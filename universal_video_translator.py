44cefb58f88caff4b9f9b06625b151eda4aa9cf3
dal1312 <127267166+dal1312@users.noreply.github.com>
2026-07-28 03:35:28 +0200

    fix: provide log streams in windowed executable

--- universal_video_translator.py
+++ universal_video_translator.py
@@ -1,5 +1,13 @@
 #!/usr/bin/env python3
+import os
+import sys
+
+if sys.stdout is None:
+    sys.stdout = open(os.devnull, "w", encoding="utf-8")
+if sys.stderr is None:
+    sys.stderr = open(os.devnull, "w", encoding="utf-8")
+
 from uvt.gui import main
 
 if __name__ == "__main__":
-    main()
+    raise SystemExit(main())
